6cae00c281ea5386e1e203280251a00ce6bcaf4d
alicfzd <alinc-ap117@code.edu.az>
2024-10-14 23:35:20 +0400

    commit1

---
+++ ClassTask/ClassTask/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    public class Product
+    {
+        public string name;
+        public string productName;
+        public decimal price;
+
+        public Product(string name, string productName, decimal price)
+        {
+            this.name = name;
+            this.productName = productName;
+            this.price = price;
+        }
+        public string Name()
+        {
+            return name;
+        }
+        public string ProductName()
+        {
+            return productName;
+        }
+        public decimal Price()
+        {
+            return price;
+        }
+
+
+
+    }
+}
---
+++ ClassTask/ClassTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[]
+            {
+            new Product("Nestle", "Coffee", 5.99m),
+            new Product("Lipton", "Çay", 1.50m),
+            new Product("7days", "Şirniyyat", 5.00m),
+            new Product("Lays", "Cips", 3.00m)
+            };
+
+            double minPrice = 1.00;
+            double maxPrice = 2.00;
+
+            PrintProducts(products, minPrice, maxPrice);
+        }
+
+        static void PrintProducts(Product[] products, double minPrice, double maxPrice)
+        {
+            foreach (var product in products)
+            {
+                decimal price = product.Price();
+                if (price >= (decimal)minPrice && price <= (decimal)maxPrice)
+                {
+                    Console.WriteLine($"Name: {product.Name()}, Product Name: {product.ProductName()}, Price: {price}");
+                }
+            }
+        }
+    }
+}
+
